83359412c0de6bcd871175b4b7ddea3309560848
Yifan Men <yifanmen@gmail.com>
2020-06-09 18:49:08 -0400

    Added for loop for testing debugger in vs code

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello World!" << "\n";
     std::cout << "This is a capstone project. The work is in progress!" << std::endl;
+    for(int i = 0;i < 10;i++) {
+        i++;
+    }
     return 0;
 }
