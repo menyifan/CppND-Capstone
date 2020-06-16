6e607c779ba5fcd7f929e1426b5682efcc91ee9a
Yifan Men <yifanmen@gmail.com>
2020-06-15 20:15:22 -0400

    Removed unused file

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yifanmen/myCode/CppND/CppND-Capstone/src/controller.cpp" "/Users/yifanmen/myCode/CppND/CppND-Capstone/build/CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
+  "/Users/yifanmen/myCode/CppND/CppND-Capstone/src/game.cpp" "/Users/yifanmen/myCode/CppND/CppND-Capstone/build/CMakeFiles/SnakeGame.dir/src/game.cpp.o"
+  "/Users/yifanmen/myCode/CppND/CppND-Capstone/src/main.cpp" "/Users/yifanmen/myCode/CppND/CppND-Capstone/build/CMakeFiles/SnakeGame.dir/src/main.cpp.o"
+  "/Users/yifanmen/myCode/CppND/CppND-Capstone/src/renderer.cpp" "/Users/yifanmen/myCode/CppND/CppND-Capstone/build/CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
+  "/Users/yifanmen/myCode/CppND/CppND-Capstone/src/snake.cpp" "/Users/yifanmen/myCode/CppND/CppND-Capstone/build/CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.12_1/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
